d486bece46c5e69e98421a62609ba128e63736c4
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-05-21 01:43:30 +0530

    zz

--- app/src/main/java/com/hack/rohanagarwal94/airtelhack/activity/CreateLoanRequest.java
+++ app/src/main/java/com/hack/rohanagarwal94/airtelhack/activity/CreateLoanRequest.java
@@ -14,10 +14,15 @@ import android.widget.MultiAutoCompleteTextView;
 import com.android.ex.chips.BaseRecipientAdapter;
 import com.android.ex.chips.RecipientEditTextView;
 import com.android.ex.chips.recipientchip.DrawableRecipientChip;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.hack.rohanagarwal94.airtelhack.PrefManager;
 import com.hack.rohanagarwal94.airtelhack.R;
+import com.hack.rohanagarwal94.airtelhack.model.Loan;
 import com.hack.rohanagarwal94.airtelhack.util.ContactPickerMulti;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by viveksb007 on 20/5/17.
@@ -26,7 +31,7 @@ import java.util.ArrayList;
 public class CreateLoanRequest extends AppCompatActivity {
 
     Button btnAddRecipient;
-    EditText etPhoneNumber, etAccountNumber;
+    EditText etTitle, etAmount;
 
 
     @Override
@@ -34,8 +39,8 @@ public class CreateLoanRequest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_loan_request);
         //btnAddRecipient = (Button) findViewById(R.id.btn_add_recipient);
-        etPhoneNumber = (EditText) findViewById(R.id.amount);
-        etAccountNumber = (EditText) findViewById(R.id.title);
+        etTitle = (EditText) findViewById(R.id.title);
+        etAmount = (EditText) findViewById(R.id.amount);
         /*
         btnAddRecipient.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,9 +76,17 @@ public class CreateLoanRequest extends AppCompatActivity {
                     //Log.v("DrawableChip", chip.getEntry().getDisplayName() + " " + chip.getEntry().getDestination());
                     phoneNumbers.add(chip.getEntry().getDestination().replace(" ", "").substring(3));
                 }
-                Log.v("Contact",phoneNumbers.toString());
+                Log.v("Contact", phoneNumbers.toString());
+                pushLoanRequest();
             }
         });
+    }
+
+    public void pushLoanRequest() {
+        PrefManager manager = new PrefManager(this);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child(manager.getNameAndNumber()[1]).child("sendLoans");
+        Loan loan = new Loan(manager.getNameAndNumber()[0], etTitle.getText().toString(), new Random().nextInt(2), Float.parseFloat(etAmount.getText().toString()));
+        reference.setValue(loan);
     }
 
     @Override
