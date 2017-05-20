f1a5a56c966b3bc5ac9056f9afc756018b182ca6
Travis CI <noreply@travis.com>
2017-05-20 17:00:48 +0530

    initial commit

---
+++ app/src/main/java/com/hack/rohanagarwal94/airtelhack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hack.rohanagarwal94.airtelhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
